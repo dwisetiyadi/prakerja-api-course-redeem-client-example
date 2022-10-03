45b8bc36898d25ff0dc17639aabdff7c565d77d9
Dwi Setiyadi <dwi.setiyadi@prakerja.go.id>
2022-10-03 16:48:42 +0700

    add python

---
+++ python/sign-key-generator.py
@@ -0,0 +1,11 @@
+from hashlib import sha1
+import hmac
+import base64
+
+# if using this value bellow should output d46b59b221335976b46eafb752035a1172a8beb2
+text = 'myclient_code1516239022POST/api/v1/integration/payment/redeem-code/commit{"code":"code"}'
+key = 'my_sign_key'
+
+result = hmac.new(key.encode("utf-8"), text.encode("utf-8"), sha1).hexdigest()
+
+print(result)
